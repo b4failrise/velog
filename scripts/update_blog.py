b12d064a2531569df8029b513254e8b27e24ecf4
Before fail occurs... <99119124+b4failrise@users.noreply.github.com>
2024-08-31 18:25:50 +0900

    Update update_blog.py
    post 수정 반영

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -33,13 +33,13 @@ for entry in feed.entries:
     file_path = os.path.join(posts_dir, file_name)
 
     # 파일이 이미 존재하지 않으면 생성
-    if not os.path.exists(file_path):
-        with open(file_path, 'w', encoding='utf-8') as file:
-            file.write(entry.description)  # 글 내용을 파일에 작성
 
-        # 깃허브 커밋
-        repo.git.add(file_path)
-        repo.git.commit('-m', f'Add post: {entry.title}')
+with open(file_path, 'w', encoding='utf-8') as file:
+    file.write(entry.description)  # 글 내용을 파일에 작성
+
+# 깃허브 커밋
+repo.git.add(file_path)
+repo.git.commit('-m', f'Add post: {entry.title}')
 
 # 변경 사항을 깃허브에 푸시
 repo.git.push()
